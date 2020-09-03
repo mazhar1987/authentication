d82c0d05154303283ed96cc5d4bebe7cbc01ee8d
swopnil <swopnil420@gmail.com>
2020-09-04 00:27:23 +0600

    Using hashing password

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 
 /**
@@ -77,7 +78,7 @@ app.route("/login")
 })
 .post(function (req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     // Find user on database and match with user type on the login page
     User.findOne({email: username}, function (err, foundUser) {
@@ -105,7 +106,7 @@ app.route("/register")
     const newUser = new User(
         {
             email: req.body.username,
-            password: req.body.password
+            password: md5(req.body.password)
         }
     );
 
